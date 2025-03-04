a472177d18dd8e7b74832b9a726caa9c1347062b
Mootae1 <1728mootae@gmail.com>
2025-03-04 17:17:23 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
+// filepath: /c:/Users/User/Desktop/MyMental/project/vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: '/Test/', // Replace <REPO_NAME> with your GitHub repository name
 });
