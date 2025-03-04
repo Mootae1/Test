430c068a9b664ae088427402d482de50d3de5269
mootae1 <1728mootae@gmail.com>
2025-03-04 21:33:38 +0700

    Fix path issues in index.html

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Test/', // Replace <REPO_NAME> with your GitHub repository name
+  base: '/Test/', // Replace with your actual repository name
 });
